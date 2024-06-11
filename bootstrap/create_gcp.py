a9e43c15b5facfbb5acba2ae8bc01bca5f87ffd9
om <ompatel1359@gmail.com>
2024-06-10 22:15:08 -0400

    almost done refactoring

--- bootstrap/create_gcp.py
+++ bootstrap/create_gcp.py
@@ -50,14 +50,12 @@ def service_accounts(project_data: PROJECT_DATA_T):
         except(Exception) as e:
             print(f"[INFO] Did not create account key for {project_id} because:")
             print(e)
-
-
         
         # create service account IAM policy binding
         try:
             client.create_role
             p = client.get_iam_policy()
-            p.       
+                 
 
 def tfstate_bucket(project_data: PROJECT_DATA_T) -> storage.Bucket: 
     storage_client = storage.Client(project=project_data["project_id_prefix"] + project_data["providers"]["general"]["project"]) #the general project
@@ -68,31 +66,3 @@ def tfstate_bucket(project_data: PROJECT_DATA_T) -> storage.Bucket:
 
     print(f"Created bucket {new_bucket.name} in {new_bucket.location} with storage class {new_bucket.storage_class}")
     return new_bucket
-
-
-#TODO: fix i dont need a seperate helper function for this 
-def create_service_account(
-    project_id: str, account_id: str, display_name: Optional[str] = None
-) -> types.ServiceAccount:
-    """
-    Creates a service account.
-
-    project_id: ID or number of the Google Cloud project you want to use.
-    account_id: ID which will be unique identifier of the service account
-    display_name (optional): human-readable name, which will be assigned to the service account
-    """
-
-    iam_admin_client = iam_admin_v1.IAMClient()
-    request = types.CreateServiceAccountRequest()
-
-    request.account_id = account_id
-    request.name = f"projects/{project_id}"
-
-    service_account = types.ServiceAccount()
-    service_account.display_name = display_name
-    request.service_account = service_account
-    account = iam_admin_client.create_service_account(request=request)
-
-    print(f"Created a service account: {account.email}")
-    return account
-
